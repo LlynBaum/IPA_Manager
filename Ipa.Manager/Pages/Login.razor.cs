69ac5dcfaa0c1b1642b10dea962497ad74d50bc1
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-12-18 14:37:39 +0100

    add login forms

--- Ipa.Manager/Pages/Login.razor.cs
+++ Ipa.Manager/Pages/Login.razor.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Components;
 
 namespace Ipa.Manager.Pages;
 
+[AllowAnonymous]
 public partial class Login : ComponentBase
 {
+    [SupplyParameterFromQuery]
+    public string? ReturnUrl { get; set; }
 }
