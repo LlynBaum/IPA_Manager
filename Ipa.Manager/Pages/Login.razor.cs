5ca9371443db4182166a7138d6268c7ca3969329
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-12-18 15:32:21 +0100

    make page switch for interactive

--- Ipa.Manager/Pages/Login.razor.cs
+++ Ipa.Manager/Pages/Login.razor.cs
@@ -4,8 +4,13 @@ using Microsoft.AspNetCore.Components;
 namespace Ipa.Manager.Pages;
 
 [AllowAnonymous]
-public partial class Login : ComponentBase
+public partial class Login(NavigationManager navigationManager) : ComponentBase
 {
     [SupplyParameterFromQuery]
     public string? ReturnUrl { get; set; }
+
+    private void ToRegister()
+    {
+        navigationManager.NavigateTo("/register");
+    }
 }
--- Ipa.Manager/Pages/Register.razor.cs
+++ Ipa.Manager/Pages/Register.razor.cs
@@ -4,8 +4,13 @@ using Microsoft.AspNetCore.Components;
 namespace Ipa.Manager.Pages;
 
 [AllowAnonymous]
-public partial class Register : ComponentBase
+public partial class Register(NavigationManager navigationManager) : ComponentBase
 {
     [SupplyParameterFromQuery]
     public string? ReturnUrl { get; set; }
+    
+    private void ToLogin()
+    {
+        navigationManager.NavigateTo("/login");
+    }
 }
