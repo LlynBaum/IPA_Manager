3663130f2cc455d935838577c7b92aa9e4493c53
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2025-12-19 09:42:07 +0100

    add migration

---
+++ Ipa.Manager/Database/Migrations/20251219084054_AddDbGeneratedIds.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ipa.Manager.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDbGeneratedIds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "first_name",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_name",
+                table: "users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "first_name",
+                table: "users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "last_name",
+                table: "users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
