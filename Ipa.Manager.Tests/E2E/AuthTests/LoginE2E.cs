30f423820bd4597078f4f9e7d771c32381dd6751
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2026-01-07 19:32:47 +0100

    Update LoginE2E.cs

--- Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
+++ Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
@@ -1,3 +1,4 @@
+using Microsoft.Playwright;
 using NUnit.Framework;
 
 namespace Ipa.Manager.Tests.E2E.AuthTests;
@@ -5,11 +6,27 @@ namespace Ipa.Manager.Tests.E2E.AuthTests;
 public class LoginE2E : PlaywrightTestBase
 {
     [Test]
-    public async Task Ka()
+    public async Task RegisterCreatesUserInDb()
     {
+        const string username = "Test";
+        const string password = "abcdefg";
+        
         await Page.GotoAsync(BaseUrl);
 
         await Expect(Page.GetByText("Login")).ToBeVisibleAsync();
         Assert.That(Page.Url, Is.EqualTo($"{BaseUrl}login?ReturnUrl=%2F"));
+
+        // TODO: fill in forms
+        await Page.GetByRole(AriaRole.Button).ClickAsync();
+        
+        Assert.That(Page.Url, Is.EqualTo(BaseUrl));
+        
+        var user = Db.Users.SingleOrDefault();
+        Assert.That(user, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(user.Username, Is.EqualTo(username));
+            Assert.That(user.PasswordHash, Is.Not.EqualTo(password));
+        });
     }
 }
