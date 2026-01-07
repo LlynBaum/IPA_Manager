16cea7485127738a22bc7189172c01ba0ce3c0f9
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2026-01-07 22:11:26 +0100

    register test

--- Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
+++ Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
@@ -5,6 +5,8 @@ namespace Ipa.Manager.Tests.E2E.AuthTests;
 
 public class LoginE2E : PlaywrightTestBase
 {
+    
+    
     [Test]
     public async Task RegisterCreatesUserInDb()
     {
@@ -16,10 +18,17 @@ public class LoginE2E : PlaywrightTestBase
         await Expect(Page.GetByText("Login")).ToBeVisibleAsync();
         Assert.That(Page.Url, Is.EqualTo($"{BaseUrl}login?ReturnUrl=%2F"));
 
-        // TODO: fill in forms
-        await Page.GetByRole(AriaRole.Button).ClickAsync();
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Sign up" }).ClickAsync();
+        await Task.Delay(100);
+        Assert.That(Page.Url, Is.EqualTo($"{BaseUrl}register?ReturnUrl=%2F"));
+
+        await Page.GetByRole(AriaRole.Textbox, new () { Name = "username" }).FillAsync(username);
+        await Page.GetByRole(AriaRole.Textbox, new () { Name = "password" }).FillAsync(password);
+        await Page.GetByRole(AriaRole.Textbox, new () { Name = "confirm-password" }).FillAsync(password);
         
-        Assert.That(Page.Url, Is.EqualTo(BaseUrl));
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
+        
+        Assert.That(Page.Url, Is.EqualTo(BaseUrl)); 
         
         var user = Db.Users.SingleOrDefault();
         Assert.That(user, Is.Not.Null);
