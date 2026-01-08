59f1e22592070229f7587268a53cd12197412236
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-01-08 10:52:45 +0100

    Update LoginE2E.cs

--- Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
+++ Ipa.Manager.Tests/E2E/AuthTests/LoginE2E.cs
@@ -1,12 +1,16 @@
 using Ipa.Manager.Tests.E2E.Framework;
 using Microsoft.Playwright;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 
 namespace Ipa.Manager.Tests.E2E.AuthTests;
 
 [TestFixture]
 public class LoginE2E : PlaywrightTestBase
 {
+    private const string Username = "Test";
+    private const string Password = "abcdefg";
+    
     protected override bool EnableTracing => true;
 
     private ILocator UsernameInput => Page.GetByRole(AriaRole.Textbox, new() { Name = "username" });
@@ -23,15 +27,12 @@ public class LoginE2E : PlaywrightTestBase
     [Test]
     public async Task Register_CreatesUserInDb()
     {
-        const string username = "Test";
-        const string password = "abcdefg";
-        
         await Page.GotoAsync(BaseUrl);
         await Page.GetByRole(AriaRole.Button, new() { Name = "Sign up" }).ClickAsync();
 
-        await UsernameInput.FillAsync(username);
-        await PasswordInput.First.FillAsync(password);
-        await PasswordInput.Last.FillAsync(password);
+        await UsernameInput.FillAsync(Username);
+        await PasswordInput.First.FillAsync(Password);
+        await PasswordInput.Last.FillAsync(Password);
         
         await PasswordInput.Last.BlurAsync();
         await Page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).ClickAsync();
@@ -42,15 +43,27 @@ public class LoginE2E : PlaywrightTestBase
         Assert.That(user, Is.Not.Null);
         Assert.Multiple(() =>
         {
-            Assert.That(user.Username, Is.EqualTo(username));
-            Assert.That(user.PasswordHash, Is.Not.EqualTo(password));
+            Assert.That(user.Username, Is.EqualTo(Username));
+            Assert.That(user.PasswordHash, Is.Not.EqualTo(Password));
         });
     }
 
     [Test]
-    public async Task Register_RedirectsBackAndDoesNotCreateUser_WhenRegisterFails()
+    public async Task Register_RedirectsBackAndDoesNotCreateUser_WhenPasswordConfirmationDoesNotMatch()
     {
+        await Page.GotoAsync(BaseUrl);
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Sign up" }).ClickAsync();
+
+        await UsernameInput.FillAsync(Username);
+        await PasswordInput.First.FillAsync(Password);
+        await PasswordInput.Last.FillAsync(Password + "Bla");
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Create Account" }).IsDisabledAsync();
         
+        Assert.That(Page.Url, Does.Contain("register"));
+        
+        var user = Db.Users.SingleOrDefault();
+        Assert.That(user, Is.Null);
     }
     
     [Test]
