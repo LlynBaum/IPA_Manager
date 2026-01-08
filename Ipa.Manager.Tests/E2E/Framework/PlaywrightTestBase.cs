e1d1861256759ed28f014ec40453936d69fb0212
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-01-08 10:20:27 +0100

    add client for integration tests

--- Ipa.Manager.Tests/E2E/Framework/PlaywrightTestBase.cs
+++ Ipa.Manager.Tests/E2E/Framework/PlaywrightTestBase.cs
@@ -7,19 +7,24 @@ using NUnit.Framework;
 namespace Ipa.Manager.Tests.E2E.Framework;
 
 [NonParallelizable]
-public class PlaywrightTestBase : PageTest
+internal class PlaywrightTestBase : PageTest
 {
     /// <summary>
     /// Use this code <code>protected override bool EnableTracing => true;</code> to Enable Playwright tracing.
     /// Playwright Tracing can be used to view the Playwright Test Snapshots. Use the following command:
     /// <code>npx playwright show-trace bin/Debug/net9.0/trace.zip</code>
     /// </summary>
-    protected virtual bool EnableTracing => false; 
+    protected virtual bool EnableTracing => false;
     
     /// <summary>
     /// The URL where the Blazor host is available.
     /// </summary>
     protected string BaseUrl = string.Empty;
+    
+    /// <summary>
+    /// A HttpClient to interact with the Web App for Integration Tests.
+    /// </summary>
+    protected HttpClient Client => PlaywrightServerFixture.Factory.Server.CreateClient();
     
     /// <summary>
     /// A DB Context for the current DB Instance. The data in the DB will be cleared after every Test run.
