99d78187b91693ec1fec781878eae0ef56c75b6d
Llyn Baumann <llyn.baumann@digitecgalaxus.ch>
2026-01-08 21:56:34 +0100

    criteria model

---
+++ Ipa.Manager/Services/Criterias/Criteria.cs
@@ -0,0 +1,9 @@
+﻿namespace Ipa.Manager.Services.Criterias;
+
+public record Criteria(string Id, string Name, string Description, IReadOnlyList<string> QualityLevels)
+{
+    public string QualityLevel1 => QualityLevels[0];
+    public string QualityLevel2 => QualityLevels[1];
+    public string QualityLevel3 => QualityLevels[2];
+    public string QualityLevel4 => QualityLevels[3];
+}
