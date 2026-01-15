e22d34a394f5e4037605b6f9ff20e6d7fb02bc1c
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2026-01-15 09:53:10 +0100

    criteria progress service

---
+++ Ipa.Manager/Services/Criterias/CriteriaProgressService.cs
@@ -0,0 +1,44 @@
+using Ipa.Manager.Database;
+using Ipa.Manager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ipa.Manager.Services.Criterias;
+
+public class CriteriaProgressService(ApplicationDbContext context, IStaticCriteriaService staticCriteria) : ICriteriaProgressService
+{
+    public async Task CreateAsync(int projectId, IReadOnlyList<string> criteriaIds)
+    {
+        foreach (var id in criteriaIds)
+        {
+            var criteriaProgress = new CriteriaProgress
+            {
+                ProjectId = projectId,
+                CriteriaId = id
+            };
+            await context.CriteriaProgress.AddAsync(criteriaProgress);
+        }
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<IReadOnlyList<ProjectCriteria>> GetByProject(int projectId)
+    {
+        var criteriasProgress = await context.CriteriaProgress
+            .Where(c => c.ProjectId == projectId)
+            .ToListAsync();
+
+        return criteriasProgress
+            .Select(c => new ProjectCriteria(c, staticCriteria.GetById(c.CriteriaId)))
+            .ToList();
+    }
+    
+    public async Task<ProjectCriteria?> GetById(int id)
+    {
+        var criteriaProgress = await context.CriteriaProgress
+            .SingleOrDefaultAsync(c => c.Id == id);
+
+        return criteriaProgress is not null 
+            ? new ProjectCriteria(criteriaProgress, staticCriteria.GetById(criteriaProgress.CriteriaId)) 
+            : null;
+    }
+}
